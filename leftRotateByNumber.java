f4ba05582fa55fcddf6830121de32a8082d67199
manideep <yelugammanideep@gmail.com>
2025-05-18 12:42:06 +0530

    first commit

--- leftRotateByNumber.java
+++ leftRotateByNumber.java
@@ -1,31 +1,44 @@
 import java.util.*;
 
 public class leftRotateByNumber {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int arr[] = {1, 2, 3, 4, 5, 6, 7};
-        int rotate = sc.nextInt();
+        System.out.println("Enter number of elements in array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+       
 
-        rotate = rotate % arr.length;
+        System.out.println("Enter left rotate number");
+        int d = sc.nextInt();
 
+        int temp[] = new int[d];
+        d = d % n;
 
-        int temp[] = new int[rotate];
+        System.out.println("Enter array numbers");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
 
-        for (int i = 0; i < rotate; i++) {
+        for(int i = 0; i < d;i++){
             temp[i] = arr[i];
         }
 
-        for (int i = rotate; i < arr.length; i++) {
-            arr[i - rotate] = arr[i];
+        for(int i = d;i < n; i++){
+            arr[i - d] = arr[i];
         }
 
-      for(int i = arr.length-rotate; i < arr.length;i++){
-          arr[i] = temp[i - (arr.length - rotate) ];
-      }
 
-      for(int i =0;i<arr.length;i++){
-        System.out.print(arr[i]);
-      }
+        for(int i = n-d ; i < n;i++){
+            arr[i] = temp[i-(n-d)];
+        }
+
+
+        for(int i = 0; i < n;i++){
+            System.out.print(arr[i]);
+        }
+       
+
 
         
     }
