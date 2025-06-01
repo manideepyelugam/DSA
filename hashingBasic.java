5b57eadd17ce5c15dddf7e60264e7108c795095d
manideep <yelugammanideep@gmail.com>
2025-06-01 21:05:30 +0530

    hashmap

--- hashingBasic.java
+++ hashingBasic.java
@@ -1,24 +1,45 @@
+import java.util.*;
+
+
 public class hashingBasic {
 
-    public static int hash(int number, int arr[]){
-
-        int count = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i] == number){
-                count++;
-            }
-        }
-
-        return count;
-
-    }
     public static void main(String[] args) {
-        int arr[] = {1,2,4,3,2,1,2};
+        HashMap<String, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < arr.length; i++) {
-          System.out.println(hash(arr[i], arr));  
+        map.put("manideep",100);
+        map.put("sathwika",120);
+
+
+        map.put("sathwika",130);
+
+
+        if(map.containsKey("manideep")){
+            System.out.println("contains manideep");
+        }else{
+            System.out.println("does not contain manideep");
         }
-        
+
+
+        System.out.println(map.get("manideep"));
+
+        System.out.println(map.get("sathvika"));
+
+        Set<String> keys = map.keySet();
+
+        for(String key : keys){
+            System.out.println(key + " " +map.get(key));
+        }
+
+
+        map.remove("manideep");
+
+
+        for(String key : keys){
+            System.out.println(key + " " +map.get(key));
+        }
+
+
+
+
     }
 }
