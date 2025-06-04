bc5dec764d68afd917e976bc2585f6419b24fb59
manideep <yelugammanideep@gmail.com>
2025-06-04 17:26:02 +0530

    sortArrayOf123

---
+++ sortArray012.java
@@ -0,0 +1,41 @@
+public class sortArray012 {
+
+    public static void main(String[] args) {
+        int a[] = {2,0,2,1,1,0};
+
+        int one = 0;
+
+        int two = 0;
+
+        int zero = 0;
+
+        
+
+        for(int i = 0; i < a.length;i++){
+           if (a[i] == 0) zero++;
+           else if(a[i] == 1) one++;
+           else two++;
+        }
+
+        int index = 0;
+
+        for (int i = 0; i < zero; i++) {
+            a[index++] = 0;
+        }
+
+        for (int i = 0; i < one; i++) {
+            a[index++] = 1;
+        }
+
+        for (int i = 0; i < two; i++) {
+            a[index++] = 2;
+        }
+
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+       
+    }
+}
