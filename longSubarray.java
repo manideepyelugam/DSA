062513e18d44d175dfb589b0c6ca2f118e1057d0
manideep <yelugammanideep@gmail.com>
2025-06-06 15:16:45 +0530

    KadanesAlgo

--- longSubarray.java
+++ longSubarray.java
@@ -1,6 +1,24 @@
 public class longSubarray {
+
+    public static int KadanesAlgo(int[] array){
+        int maxSum = 0;
+        int currentSum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            currentSum += array[i];
+            maxSum = Math.max(maxSum, currentSum);
+            if(currentSum < 0){
+                currentSum = 0;
+            }
+
+            
+        }
+
+        return maxSum;
+    }
+
     public static void main(String[] args) {
-        int k[] = {-2,1,-3,4,-1,2,1,-5,4};
+        int k[] = {-2,1,-3,4,-1,2,1,-5,4,9};
 
         int maxSum = 0;
         int len = 0;
@@ -19,6 +37,9 @@ public class longSubarray {
             }
         }
 
-        System.out.println(maxSum +" "+ len);
+        System.out.println(maxSum);
+
+         
+        System.out.println("maxsum from kadanes algo"+" "+ KadanesAlgo(k));
     }
 }
