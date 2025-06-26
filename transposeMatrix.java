55c6a8947944e213877736f5429763f4e0645c52
manideep <yelugammanideep@gmail.com>
2025-06-26 15:18:21 +0530

    transpose matrix

---
+++ transposeMatrix.java
@@ -0,0 +1,32 @@
+public class transposeMatrix {
+    public static void main(String[] args) {
+        int array[][] = {{1,2,3}
+                        ,{4,5,6}
+                        ,{7,8,9}};
+
+
+        
+        
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i+1; j < array.length; j++) {
+                if(i != j){
+
+                   int temp = array[i][j];
+                   array[i][j] = array[j][i];
+                   array[j][i] = temp;
+
+                }
+            }
+        }
+
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
